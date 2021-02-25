bf202369e2741c68ded4a8db16551b81cb15d234
Iryna Feuerstein <iryna.feuerstein@trivago.com>
2021-02-25 07:54:02 +0100

    Initial commit

---
+++ src/main/java/com/irare/climate/change/measurementsprovider/MeasurementsProviderApplication.java
@@ -0,0 +1,13 @@
+package com.irare.climate.change.measurementsprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementsProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementsProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irare/climate/change/measurementsprovider/MeasurementsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irare.climate.change.measurementsprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementsProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
